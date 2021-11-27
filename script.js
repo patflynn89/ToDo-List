55e15e91403dbd457cf103265c216523e6fb82b9
patflynn89 <xbl_bachhuber@hotmail.com>
2021-11-27 15:24:35 +0100

    ActionListener added

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const submitButton = document.getElementById("taskToDo");
+const submitField = document.getElementById("taskToDo");
 const taskBox = document.getElementById("task");
 const listItemsCSS = document.getElementById("list");
-
+let input = document.getElementById("task").value;
+/**
 function taskButton() {
   if (taskBox && taskBox.value) {
     //alert(taskBox);
     listItemsCSS.style.display = "";
-
+ 
   }
-
+ 
 }
+ */
 
-taskToDo.addEventListener("click", taskButton);
+submitField.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Yeah mann funktionier!" + e.input);
+});
