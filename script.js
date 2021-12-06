751024fd34500bd41f0a3d5d06acf72a3729c61d
Martin Bachhuber <martin@adf-solutions.local>
2021-12-06 10:26:13 +0100

    Added among others the edit and save button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const submitField = document.getElementById("taskToDo");
-const listItemsCSS = document.getElementById("list");
+const fields = document.getElementById("fields");
 
 /* document.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -10,11 +10,24 @@ const listItemsCSS = document.getElementById("list");
 submitField.addEventListener("click", function (e) {
   e.preventDefault();
   const taskBox = document.getElementById("taskForm").elements["task"].value;
-  const node = document.createElement("li");
+  const div = document.createElement("div");
+  const node = document.createElement("label");
   const textNode = document.createTextNode(taskBox);
+  const editButton = document.createElement("button");
+  const saveButton = document.createElement("button");
+  editButton.id = "buttonBox";
+  node.setAttribute("type", "text");
+  node.setAttribute("value", taskBox);
+  node.setAttribute("id", "taskToDo");
+  div.id = "boxes";
+  editButton.innerHTML = "Edit";
+  saveButton.innerHTML = "Save";
   //alert("Yeah mann funktionier! " + taskBox);
   node.appendChild(textNode);
-  listItemsCSS.appendChild(node);
+  div.appendChild(node);
+  div.appendChild(editButton);
+  div.appendChild(saveButton);
+  fields.appendChild(div);
   clearBox();
 });
 
